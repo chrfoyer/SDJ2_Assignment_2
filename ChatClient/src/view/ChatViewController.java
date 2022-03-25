20647ccd4427ee1b12d5ec664f38b3a8f650006b
KrunoNeric <91593987+KrunoNeric@users.noreply.github.com>
2022-03-25 13:34:09 +0100

    GUI_v0.2

--- ChatClient/src/view/ChatViewController.java
+++ ChatClient/src/view/ChatViewController.java
@@ -1,9 +1,28 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class ChatViewController extends ViewController
 {
+  @FXML private Label errorLabel;
+  @FXML private TextField inputField;
+  @FXML private ListView listView;
   @Override protected void init()
   {
+    errorLabel.textProperty().set("");
+  }
 
+  public void logout()
+  {
+    getViewHandler().openView("LoginView.fxml");
+  }
+
+  public void send()
+  {
+    //
   }
 }
