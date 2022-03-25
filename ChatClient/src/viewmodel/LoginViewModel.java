28cd9f94a9482a579d82cc53102be149c90372c5
Raedrim <43709947+Raedrim@users.noreply.github.com>
2022-03-25 10:12:20 +0100

    VM v0.1

--- ChatClient/src/viewmodel/LoginViewModel.java
+++ ChatClient/src/viewmodel/LoginViewModel.java
@@ -1,5 +1,42 @@
 package viewmodel;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class LoginViewModel
 {
+  private Model model;
+  private StringProperty request;
+  private StringProperty reply;
+  private StringProperty error;
+
+  public LoginViewModel(Model model)
+  {
+    this.model = model;
+    error = new SimpleStringProperty();
+    request = new SimpleStringProperty();
+    reply = new SimpleStringProperty();
+  }
+
+  public void login()
+  {
+    try
+    {
+      //call setUsername on server
+    }
+    catch (Exception e)
+    {
+      error.set(e.getMessage());
+    }
+  }
+
+  public StringProperty errorProperty()
+  {
+    return error;
+  }
+
+  public StringProperty requestProperty()
+  {
+    return request;
+  }
 }
--- ChatClient/src/viewmodel/ViewModelFactory.java
+++ ChatClient/src/viewmodel/ViewModelFactory.java
@@ -2,4 +2,25 @@ package viewmodel;
 
 public class ViewModelFactory
 {
+
+  private LoginViewModel loginViewModel;
+  private ChatViewModel chatViewModel;
+
+  public ViewModelFactory(Model model)
+  {
+    chatViewModel = new ChatViewModel(model);
+    loginViewModel = new LoginViewModel(model);
+
+  }
+
+  public ChatViewModel getChatViewModel()
+  {
+    return chatViewModel;
+  }
+
+  public LoginViewModel getLoginViewModel()
+  {
+    return loginViewModel;
+  }
+
 }
